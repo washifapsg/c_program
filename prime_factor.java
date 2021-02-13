f0c8b72eef998ef5d7653d6cd7257f8f4d1f1f46
washifapsg <67793201+washifapsg@users.noreply.github.com>
2021-02-13 21:43:31 +0530

    Create prime_factor.java

---
+++ prime_factor.java
@@ -0,0 +1,32 @@
+
+# include <stdio.h> 
+# include <math.h> 
+
+void primeFactors(int n) 
+{ 
+	while (n%2 == 0) 
+	{ 
+		printf("%d ", 2); 
+		n = n/2; 
+	} 
+	int i;
+	for(i = 3; i <= sqrt(n); i = i+2) 
+	{  
+		while (n%i == 0) 
+		{ 
+			printf("%d ", i); 
+			n = n/i; 
+		} 
+	} 
+	if (n > 2) 
+		printf ("%d ", n); 
+} 
+
+int main() 
+{ 
+	int n;
+	printf("enter the number"); 
+	scanf("%d",&n);
+	primeFactors(n); 
+	return 0; 
+} 
